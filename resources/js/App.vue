af13e9952bf14ec57c6c6d36c85271903b889761
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-03-02 23:15:28 +0300

    update home ui :art:

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <main class="max-w-screen-md mx-auto py-4 px-4">
         <router-view :key="$route.path" />
-    </div>
+    </main>
 </template>
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,5 +2,12 @@
 </script>
 
 <template>
-    <h1>Home</h1>
+    <header class="py-8 text-center">
+        <h1 class="mb-4 text-2xl tracking-tight text-slate-900 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-purple-600 sm:text-4xl">
+            My Awesome Blog
+        </h1>
+        <p class="text-md text-slate-600 sm:text-lg">
+            All the latest Laravel and Vue.js news from another human being 👋
+        </p>
+    </header>
 </template>
